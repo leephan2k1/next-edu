f8eff14affc05fc78d41fcbf3cdebd2e0499cf06
leephan2k1 <pttl2k1@gmail.com>
2022-12-24 11:47:21 +0700

    init custom configs

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<main className="bg-white">Init Next Edu App</main>
+		</>
+	);
+};
+
+export default Home;
