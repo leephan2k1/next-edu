16c84026566192e0b93d9069c0b63e623fb703ba
leephan2k1 <pttl2k1@gmail.com>
2023-01-30 22:46:28 +0700

    style: 💄 add section followed courses

---
+++ src/components/partials/FollowedCourses.tsx
@@ -0,0 +1,31 @@
+import { memo } from 'react';
+import ModernCourseCard from '../shared/ModernCourseCard';
+
+function FollowedCourses() {
+  return (
+    <div className="w-full">
+      <button className="smooth-effect my-4 w-fit rounded-xl bg-rose-400 p-4 text-xl text-white">
+        Huỷ yêu thích tất cả
+      </button>
+      <ul className="grid w-full grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+        <li>
+          <ModernCourseCard />
+        </li>
+        <li>
+          <ModernCourseCard />
+        </li>
+        <li>
+          <ModernCourseCard />
+        </li>
+        <li>
+          <ModernCourseCard />
+        </li>
+        <li>
+          <ModernCourseCard />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default memo(FollowedCourses);
