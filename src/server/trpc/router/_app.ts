2c6bf9db5ad4bf5f46e5e67c35c4c6959f078483
leephan2k1 <pttl2k1@gmail.com>
2023-02-24 23:21:21 +0700

    feat: 🎸 add course router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
-import { router } from "../trpc";
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { router } from '../trpc';
+import { authRouter } from './auth';
+import { exampleRouter } from './example';
+import { courseRouter } from './course';
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  course: courseRouter,
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/course.ts
@@ -0,0 +1,47 @@
+import { z } from 'zod';
+import { router, protectedProcedure, publicProcedure } from '../trpc';
+
+export const courseRouter = router({
+  findCoursesByOwner: protectedProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const { userId } = input;
+      const courses = await ctx.prisma.course.findMany({
+        where: { userId },
+        select: {
+          id: true,
+          name: true,
+          createdAt: true,
+          updatedAt: true,
+          slug: true,
+          verified: true,
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      });
+
+      return { message: 'success', courses };
+    }),
+
+  findCourseBySlug: publicProcedure
+    .input(z.object({ slug: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const { slug } = input;
+
+      const course = await ctx.prisma.course.findUnique({
+        where: { slug },
+        include: {
+          courseTargets: { distinct: ['content'] },
+          courseRequirements: { distinct: ['content'] },
+          chapters: true,
+          reviews: true,
+          students: true,
+          instructor: true,
+          category: true,
+        },
+      });
+
+      return course;
+    }),
+});
