263d0e02ccfa051076e8f54a1d2459828c36db75
leephan2k1 <pttl2k1@gmail.com>
2023-02-28 03:57:20 +0700

    feat: 🎸 fetching course

--- src/pages/course/[slug].tsx
+++ src/pages/course/[slug].tsx
@@ -1,24 +1,76 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticPaths, GetStaticProps } from 'next';
 import CourseHeader from '~/components/courses/CourseHeader';
 import BuyOnly from '~/components/partials/BuyOnly';
 import CourseBody from '~/components/courses/CourseBody';
 import CourseFooter from '~/components/courses/CourseFooter';
 import CommentModal from '~/components/shared/CommentModal';
+import { prisma } from '~/server/db/client';
+import type { CourseType } from '~/types';
 
-const CoursePage: NextPage = () => {
+interface CoursePageProps {
+  course: CourseType;
+}
+
+const CoursePage: NextPage<CoursePageProps> = ({ course }) => {
   return (
     <div className="min-h-screen">
-      <CourseHeader />
+      <CourseHeader course={course} />
 
-      <CourseBody />
+      {course ? (
+        <>
+          <CourseBody />
 
-      <CourseFooter />
+          <CourseFooter />
 
-      <CommentModal />
+          <CommentModal />
 
-      <BuyOnly />
+          <BuyOnly />
+        </>
+      ) : null}
     </div>
   );
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const { slug } = params;
+
+  const course = await prisma.course.findUnique({
+    where: { slug },
+    include: {
+      courseTargets: { distinct: ['content'] },
+      courseRequirements: { distinct: ['content'] },
+      chapters: {
+        include: {
+          lectures: {
+            include: {
+              resources: true,
+              discussions: true,
+              learnedBy: true,
+            },
+          },
+        },
+      },
+      reviews: true,
+      students: true,
+      instructor: true,
+      category: true,
+    },
+  });
+
+  if (!course) return { notFound: true };
+
+  return {
+    props: { course: JSON.parse(JSON.stringify(course)) },
+    revalidate: 60 * 60 * 12, //12h
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return { paths: [], fallback: true };
 };
 
 export default CoursePage;
