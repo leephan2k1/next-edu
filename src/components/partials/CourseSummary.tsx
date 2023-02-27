462fdaf6ca7d7850042118cf0cee3b307c17669f
leephan2k1 <pttl2k1@gmail.com>
2023-02-28 02:18:45 +0700

    feat: 🎸 fetching & render cards list

--- src/components/partials/CourseSummary.tsx
+++ src/components/partials/CourseSummary.tsx
@@ -1,10 +1,18 @@
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { RiDraftLine } from 'react-icons/ri';
 import { SiGoogleclassroom } from 'react-icons/si';
 import { VscTasklist } from 'react-icons/vsc';
+import { Else, If, Then } from 'react-if';
 import { PATHS } from '~/constants';
+import { trpc } from '~/utils/trpc';
+
+import Loading from '../buttons/Loading';
 import ModernCourseCard from '../shared/ModernCourseCard';
 import CoursesDraft from './CoursesDraft';
+
+import type { ReactNode } from 'react';
+import type { VerifiedStateType } from '~/types';
 
 export default function CourseCreation() {
   const router = useRouter();
@@ -22,30 +30,13 @@ export default function CourseCreation() {
           Tạo khoá học
         </button>
       </div>
-      <div className="mx-auto flex w-[90%] flex-col md:w-[80%]">
+
+      <CourseList verified={'APPROVED'}>
         <h1 className="flex space-x-4 text-3xl">
           <SiGoogleclassroom className="h-8 w-8" />{' '}
           <span className="font-bold">Khoá học đang hướng dẫn</span>
         </h1>
-        {/* <h4 className="my-4 italic">Bạn chưa hướng dẫn khoá học nào!</h4> */}
-        <ul className="grid w-full grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
-          <li>
-            <ModernCourseCard />
-          </li>
-          <li>
-            <ModernCourseCard />
-          </li>
-          <li>
-            <ModernCourseCard />
-          </li>
-          <li>
-            <ModernCourseCard />
-          </li>
-          <li>
-            <ModernCourseCard />
-          </li>
-        </ul>
-      </div>
+      </CourseList>
 
       <div className="mx-auto flex w-[90%] flex-col overflow-x-scroll md:w-[80%]">
         <h1 className="flex space-x-4 text-3xl">
@@ -56,30 +47,68 @@ export default function CourseCreation() {
         <CoursesDraft />
       </div>
 
-      <div className="mx-auto flex w-[90%] flex-col md:w-[80%]">
+      <CourseList verified={'PENDING'}>
         <h1 className="flex space-x-4 text-3xl">
           <VscTasklist className="h-8 w-8" />{' '}
           <span className="font-bold">Khoá học đang chờ phê duyệt</span>
         </h1>
-        {/* <h4 className="my-4 italic">Bạn chưa hướng dẫn khoá học nào!</h4> */}
-        <ul className="grid w-full grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
-          <li>
-            <ModernCourseCard />
-          </li>
-          <li>
-            <ModernCourseCard />
-          </li>
-          <li>
-            <ModernCourseCard />
-          </li>
-          <li>
-            <ModernCourseCard />
-          </li>
-          <li>
-            <ModernCourseCard />
-          </li>
-        </ul>
-      </div>
+      </CourseList>
+    </div>
+  );
+}
+
+function CourseList({
+  children,
+  verified,
+}: {
+  children: ReactNode;
+  verified: VerifiedStateType;
+}) {
+  const { data: session } = useSession();
+
+  const { data: courses, isLoading } =
+    trpc.course.findWaitingListCourses.useQuery({
+      userId: session?.user?.id as string,
+      published: true,
+      verified,
+    });
+
+  if (Array.isArray(courses) && courses.length === 0) {
+    return <h4 className="my-4 italic">Chưa có khoá học nào!</h4>;
+  }
+
+  return (
+    <div className="mx-auto flex w-[90%] flex-col md:w-[80%]">
+      {children}
+      <If condition={isLoading || !courses}>
+        <Then>
+          <div className="full-size absolute-center h-[15rem]">
+            <Loading />
+          </div>
+        </Then>
+        <Else>
+          <ul className="grid w-full grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
+            {Array.isArray(courses) &&
+              courses.length > 0 &&
+              courses.map((course) => {
+                return (
+                  <li key={course.id}>
+                    <ModernCourseCard
+                      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                      // @ts-ignore
+                      course={course}
+                      path={
+                        verified === 'PENDING'
+                          ? `/${PATHS.TEACHING}/${PATHS.EDIT_COURSE}?slug=${course.slug}`
+                          : `/${PATHS.COURSE}/${course.slug}`
+                      }
+                    />
+                  </li>
+                );
+              })}
+          </ul>
+        </Else>
+      </If>
     </div>
   );
 }
--- src/components/shared/ModernCourseCard.tsx
+++ src/components/shared/ModernCourseCard.tsx
@@ -4,22 +4,34 @@ import { useElementSize } from 'usehooks-ts';
 import { BsStarFill } from 'react-icons/bs';
 import { HeartIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { PATHS } from '~/constants';
 
-function ModernCourseCard() {
+import type { CourseType } from '~/types';
+import { MAPPING_LEVEL_LANGUAGE } from '~/constants';
+
+interface ModernCourseCardProps {
+  shouldShowFvBtn?: boolean;
+  shouldRating?: boolean;
+  course: CourseType;
+  path: string;
+}
+
+function ModernCourseCard({
+  path,
+  course,
+  shouldShowFvBtn,
+  shouldRating,
+}: ModernCourseCardProps) {
   const [imageRef, { height }] = useElementSize();
 
   return (
-    <Link href={`/${PATHS.COURSE}/courseId`} className="full-size">
+    <Link href={path} className="full-size">
       <div className="aspect-h-6 aspect-w-5 relative mt-16 rounded-2xl bg-white shadow-xl dark:bg-dark-background">
         <div className="absolute -top-[10%] left-0 mx-auto max-h-[30%] max-w-[85%]">
           <figure className="relative overflow-hidden rounded-2xl pb-[56.25%]">
             <Image
               ref={imageRef}
               className=""
-              src={
-                'https://img-b.udemycdn.com/course/750x422/1565838_e54e_16.jpg'
-              }
+              src={course.thumbnail || ''}
               fill
               alt="course-thumbnail"
             />
@@ -31,49 +43,53 @@ function ModernCourseCard() {
             height: `calc(110% - ${height}px)`,
             marginTop: `calc(${height}px - 10%)`,
           }}
-          className={`mx-auto flex w-[85%] flex-col py-2`}
+          className={`mx-auto flex w-[85%] flex-col py-2 lg:space-y-4`}
         >
           <div className="flex w-full items-center justify-between">
             <span className="rounded-lg bg-gray-500 px-3 py-2 text-sm text-white dark:bg-white dark:text-gray-600 md:text-base">
-              Beginner
+              {Object.keys(MAPPING_LEVEL_LANGUAGE).find(
+                (key) => MAPPING_LEVEL_LANGUAGE[key] === course.courseLevel,
+              )}
             </span>
 
             <span className="text-base font-bold md:text-lg lg:text-xl">
               {new Intl.NumberFormat('en', { notation: 'compact' }).format(
-                1_522_00,
+                course.coursePrice as number,
               )}{' '}
               vnđ
             </span>
           </div>
 
           <h1 className="mt-2 text-xl font-bold line-clamp-2 md:text-2xl md:line-clamp-1 lg:line-clamp-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            nulla expedita a doloribus accusamus reprehenderit minima natus,
-            obcaecati consequuntur ea libero tenetur vitae ut, voluptatibus ipsa
-            doloremque odio eos provident!
+            {course.name}
           </h1>
 
-          <h2 className="mb-2 text-lg line-clamp-1 md:my-2">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
-            alias nihil nulla nostrum hic ipsa laborum corporis consectetur
-            facere repudiandae ratione debitis enim porro tempore, quasi cum
-            nemo suscipit exercitationem.
+          <h2
+            className={`mb-2 text-lg ${
+              shouldRating ? 'line-clamp-1' : 'line-clamp-2'
+            } md:my-2`}
+          >
+            {course.briefDescription}
           </h2>
 
-          <div className="flex w-full items-center space-x-2 text-sm md:text-base lg:text-xl">
-            <span className="mr-4 ">5.0</span>
-            <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
-            <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
-            <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
-            <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
-            <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
-          </div>
+          {shouldRating && (
+            <div className="flex w-full items-center space-x-2 text-sm md:text-base lg:text-xl">
+              <span className="mr-4 ">5.0</span>
+              <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
+              <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
+              <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
+              <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
+              <BsStarFill className="h-3 w-3 text-yellow-300 md:h-5 md:w-5" />
+            </div>
+          )}
 
-          <div className="my-auto flex w-full items-center justify-end lg:mt-auto lg:pb-2">
-            <button className="btn-follow-theme btn-sm btn-circle btn hover:bg-rose-100 hover:text-rose-500 md:btn-md">
-              <HeartIcon className="h-4 w-4 md:h-6 md:w-6" />
-            </button>
-          </div>
+          {shouldShowFvBtn && (
+            <div className="my-auto flex w-full items-center justify-end lg:mt-auto lg:pb-2">
+              <button className="btn-follow-theme btn-sm btn-circle btn hover:bg-rose-100 hover:text-rose-500 md:btn-md">
+                <HeartIcon className="h-4 w-4 md:h-6 md:w-6" />
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </Link>
