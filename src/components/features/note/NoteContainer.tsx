5d30a15d68a0a26e346b7bc181b39a709c1ad7e5
leephan2k1 <pttl2k1@gmail.com>
2023-03-02 21:58:28 +0700

    feat: 🎸 handle submit note

--- src/components/features/note/NoteContainer.tsx
+++ src/components/features/note/NoteContainer.tsx
@@ -1,13 +1,61 @@
-import { memo } from 'react';
+import { memo, useRef, useEffect } from 'react';
 import Editor from '~/components/shared/Editor';
 import CurrentTimeBtn from './CurrentTimeBtn';
 import ClientOnly from '~/components/shared/ClientOnly';
 import { ListBulletIcon } from '@heroicons/react/24/outline';
 import { useSetAtom } from 'jotai';
 import { listNoteModalState } from '~/atoms/listNoteModal';
+import type QuillComponent from 'react-quill';
+import useLecture from '~/contexts/LearningContext';
+import { videoCurrentTime } from '~/atoms/videoCurrentTime';
+import { useAtomValue } from 'jotai';
+import { trpc } from '~/utils/trpc';
+import toast from 'react-hot-toast';
 
 function NoteContainer() {
+  const videoCurrentTimeValue = useAtomValue(videoCurrentTime);
+  const editorRef = useRef<QuillComponent | null>(null);
+  const lectureCtx = useLecture();
+
   const openListNoteModal = useSetAtom(listNoteModalState);
+
+  const {
+    mutate: addNote,
+    isSuccess,
+    isError,
+    isLoading,
+  } = trpc.user.addNote.useMutation();
+
+  const handleSubmitNote = () => {
+    const payload = {
+      content: editorRef.current?.value,
+      chapterId: lectureCtx?.currentLecture?.chapterId,
+      lectureId: lectureCtx?.currentLecture?.id,
+      notchedAt: videoCurrentTimeValue,
+    };
+
+    for (const key in payload) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      if (!payload[key]) {
+        toast.error('Oops! Có lỗi xảy ra, thử lại sau!');
+        return;
+      }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    addNote(payload);
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success('Thêm ghi chú thành công');
+    }
+    if (isError) {
+      toast.error('Oops! Có lỗi xảy ra, thử lại sau!');
+    }
+  }, [isSuccess, isError]);
 
   return (
     <>
@@ -26,7 +74,17 @@ function NoteContainer() {
       </div>
 
       <ClientOnly>
-        <Editor />
+        <Editor
+          key={String(isSuccess)}
+          contents={isSuccess ? '' : undefined}
+          isLoadingSubmit={isLoading}
+          onSubmit={() => {
+            handleSubmitNote();
+          }}
+          getInstance={(editor) => {
+            editorRef.current = editor;
+          }}
+        />
       </ClientOnly>
     </>
   );
