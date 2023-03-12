44bee5fff3ed37325812de20fa4682693323a40c
leephan2k1 <pttl2k1@gmail.com>
2023-03-13 05:07:44 +0700

    refactor: 💡 sperate component

---
+++ src/components/features/teaching/TeachingSidebar.tsx
@@ -0,0 +1,64 @@
+import DashBoardSidebar from '~/components/partials/DashBoardSidebar';
+
+import {
+  ChartBarIcon,
+  InformationCircleIcon,
+} from '@heroicons/react/24/outline';
+import { useRouter } from 'next/router';
+import { AiOutlineFundProjectionScreen } from 'react-icons/ai';
+import { PATHS } from '~/constants';
+
+export default function TeachingSidebar() {
+  const router = useRouter();
+
+  return (
+    <DashBoardSidebar>
+      <button
+        onClick={() => {
+          router.replace(`/${PATHS.TEACHING}/${PATHS.COURSE}`);
+        }}
+        className={`smooth-effect flex flex-col items-center space-y-2 rounded-2xl p-4 hover:bg-slate-200 hover:dark:bg-black ${
+          router.query?.params &&
+          router.query.params[0] === PATHS.COURSE &&
+          'bg-slate-200 dark:bg-black'
+        }`}
+      >
+        <AiOutlineFundProjectionScreen className="h-10 w-10" />
+        <span>Khoá học</span>
+      </button>
+
+      <button
+        onClick={() => {
+          router.replace(`/${PATHS.TEACHING}/${PATHS.DASHBOARD}`);
+        }}
+        className={`smooth-effect flex flex-col items-center space-y-2 rounded-2xl p-4 hover:bg-slate-200 hover:dark:bg-black ${
+          router.query?.params &&
+          router.query.params[0] === PATHS.DASHBOARD &&
+          'bg-slate-200 dark:bg-black'
+        }`}
+      >
+        <ChartBarIcon className="h-10 w-10" />
+        <span>Phân tích</span>
+      </button>
+
+      {/* <button className="smooth-effect flex flex-col items-center space-y-2 rounded-2xl p-4 hover:bg-slate-200 hover:dark:bg-black">
+        <VscOrganization className="h-10 w-10" />
+        <span>Tổ chức</span>
+      </button> */}
+
+      <button
+        onClick={() => {
+          router.replace(`/${PATHS.TEACHING}/${PATHS.INSTRUCTIONS}`);
+        }}
+        className={`smooth-effect flex flex-col items-center space-y-2 rounded-2xl p-4 hover:bg-slate-200 hover:dark:bg-black ${
+          router.query?.params &&
+          router.query.params[0] === PATHS.INSTRUCTIONS &&
+          'bg-slate-200 dark:bg-black'
+        }`}
+      >
+        <InformationCircleIcon className="h-10 w-10" />
+        <span>Hướng dẫn</span>
+      </button>
+    </DashBoardSidebar>
+  );
+}
--- src/pages/teaching/[...params].tsx
+++ src/pages/teaching/[...params].tsx
@@ -1,48 +1,22 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { Case, Default, Switch } from 'react-if';
+import TeachingDashboard from '~/components/features/teaching/TeachingDashboard';
+import TeachingSidebar from '~/components/features/teaching/TeachingSidebar';
 import MainLayout from '~/components/layouts/MainLayout';
 import CourseCreation from '~/components/partials/CourseCreation';
 import CourseSummary from '~/components/partials/CourseSummary';
-import DashBoardSidebar from '~/components/partials/DashBoardSidebar';
 import { PATHS } from '~/constants';
 
-import {
-  ChartBarIcon,
-  InformationCircleIcon,
-} from '@heroicons/react/24/outline';
-import { AiOutlineFundProjectionScreen } from 'react-icons/ai';
-import { VscOrganization } from 'react-icons/vsc';
-
-const TeachingDashboard: NextPage = () => {
+const TeachingDashboardPage: NextPage = () => {
   const router = useRouter();
 
   return (
     <div className="relative min-h-screen text-gray-600 dark:text-white md:pl-[16rem]">
-      <DashBoardSidebar>
-        <button className="smooth-effect flex flex-col items-center space-y-2 rounded-2xl bg-slate-200 p-4 dark:bg-black">
-          <AiOutlineFundProjectionScreen className="h-10 w-10" />
-          <span>Khoá học</span>
-        </button>
-
-        <button className="smooth-effect flex flex-col items-center space-y-2 rounded-2xl p-4 hover:bg-slate-200 hover:dark:bg-black">
-          <ChartBarIcon className="h-10 w-10" />
-          <span>Phân tích</span>
-        </button>
-
-        <button className="smooth-effect flex flex-col items-center space-y-2 rounded-2xl p-4 hover:bg-slate-200 hover:dark:bg-black">
-          <VscOrganization className="h-10 w-10" />
-          <span>Tổ chức</span>
-        </button>
-
-        <button className="smooth-effect flex flex-col items-center space-y-2 rounded-2xl p-4 hover:bg-slate-200 hover:dark:bg-black">
-          <InformationCircleIcon className="h-10 w-10" />
-          <span>Hướng dẫn</span>
-        </button>
-      </DashBoardSidebar>
+      <TeachingSidebar />
 
       <Switch>
-        <Case condition={router.asPath.includes('dashboard')}>
+        <Case condition={router.asPath.includes(PATHS.COURSE)}>
           <CourseSummary />
         </Case>
 
@@ -55,9 +29,11 @@ const TeachingDashboard: NextPage = () => {
           <CourseCreation />
         </Case>
 
-        <Default>
-          <div></div>
-        </Default>
+        <Case condition={router.asPath.includes(PATHS.DASHBOARD)}>
+          <TeachingDashboard />
+        </Case>
+
+        <Default>{null}</Default>
       </Switch>
     </div>
   );
@@ -66,8 +42,8 @@ const TeachingDashboard: NextPage = () => {
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // -> There is no need for a header & a footer.
-TeachingDashboard.getLayout = (page: ReactNode) => {
+TeachingDashboardPage.getLayout = (page: ReactNode) => {
   return <MainLayout>{page}</MainLayout>;
 };
 
-export default TeachingDashboard;
+export default TeachingDashboardPage;
