385819419413ff9d642f40141848ef4757ec36dd
leephan2k1 <pttl2k1@gmail.com>
2023-03-22 22:49:42 +0700

    refactor: 💡 edit DateSelect component

--- src/components/shared/DateSelect.tsx
+++ src/components/shared/DateSelect.tsx
@@ -1,17 +1,39 @@
 import { useState } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
 
 const DayItem = ({
   label,
   selectedProps,
+  setSelections,
 }: {
   label: string;
   selectedProps: boolean;
+  setSelections: Dispatch<
+    SetStateAction<
+      {
+        timeLabel: string;
+        selected: boolean;
+        timeValue: string;
+      }[]
+    >
+  >;
 }) => {
   const [selected, setSelected] = useState(selectedProps);
   return (
     <>
       <button
-        onClick={() => setSelected((prevState) => !prevState)}
+        onClick={() => {
+          setSelected((prevState) => !prevState);
+          setSelections((selections) =>
+            selections.map((s) => {
+              if (s.timeLabel === label) {
+                return { ...s, selected: !s.selected };
+              }
+
+              return s;
+            }),
+          );
+        }}
         className={`${
           selected
             ? 'bg-yellow-500 text-white'
@@ -26,14 +48,24 @@ const DayItem = ({
 
 interface DateSelectProps {
   data: { timeLabel: string; selected: boolean }[];
+  setSelections: Dispatch<
+    SetStateAction<
+      {
+        timeLabel: string;
+        selected: boolean;
+        timeValue: string;
+      }[]
+    >
+  >;
 }
 
-export default function DateSelect({ data }: DateSelectProps) {
+export default function DateSelect({ data, setSelections }: DateSelectProps) {
   return (
     <div className="my-4 flex space-x-4">
       {data.map((time) => {
         return (
           <DayItem
+            setSelections={setSelections}
             label={time.timeLabel}
             key={time.timeLabel}
             selectedProps={time.selected}
