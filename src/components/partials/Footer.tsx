78c209cbc1cae8573223a77b5dc6f8b4940c4270
leephan2k1 <pttl2k1@gmail.com>
2023-03-25 12:19:17 +0700

    style: 💄 design footer

--- src/components/partials/Footer.tsx
+++ src/components/partials/Footer.tsx
@@ -1,54 +1,100 @@
+import { AiFillTwitterCircle, AiOutlineGithub } from 'react-icons/ai';
+import { BsFacebook } from 'react-icons/bs';
+import { inter } from '~/constants';
+import Balancer from 'react-wrap-balancer';
+
+import Logo from './Logo';
+
 export default function Footer() {
   return (
-    <>
-      <footer className="footer footer-center mt-10 rounded bg-[#efeae6] p-10 text-2xl text-gray-600 dark:bg-dark-background dark:text-white/50 md:text-3xl">
-        <div className="grid grid-flow-col gap-4">
-          <a className="link-hover link">Về chúng tôi</a>
-          <a className="link-hover link">Điều khoản dịch vụ</a>
-          <a className="link-hover link">Chính sách riêng tư</a>
-          <a className="link-hover link">Liên hệ</a>
-        </div>
-        <div>
-          <div className="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
-          </div>
-        </div>
-        <div>
-          <p>Copyright © 2023 - All right reserved by Next Edu</p>
-        </div>
-      </footer>
-    </>
+    <footer className="mt-10 flex flex-col items-center rounded bg-[#efeae6] px-10 py-20 text-2xl text-gray-600 dark:bg-dark-background dark:text-white/50 md:text-3xl">
+      <Logo />
+
+      <small className="my-1 text-sm font-semibold">
+        Powered by{' '}
+        <a
+          className="text-blue-400"
+          target="_blank"
+          rel="noreferrer"
+          href="https://create.t3.gg/"
+        >
+          T3-Stack
+        </a>
+      </small>
+
+      <div className="flex items-center space-x-6">
+        <a
+          href={'https://github.com/leephan2k1/next-edu'}
+          target="_blank"
+          rel="noreferrer"
+          className="my-4"
+        >
+          <AiOutlineGithub className="h-9 w-9" />
+        </a>
+
+        <a
+          href={'https://www.facebook.com/leephan2001'}
+          target="_blank"
+          rel="noreferrer"
+          className="my-4"
+        >
+          <BsFacebook className="h-8 w-8" />
+        </a>
+
+        <a
+          href={'https://twitter.com/trietly7'}
+          target="_blank"
+          rel="noreferrer"
+          className="my-4"
+        >
+          <AiFillTwitterCircle className="h-9 w-9" />
+        </a>
+      </div>
+      <h1
+        className="mt-4 max-w-[60%] text-center text-xl font-bold text-gray-500 dark:text-white/60 md:max-w-[40%] md:text-3xl"
+        style={{
+          fontFamily: inter.style.fontFamily,
+        }}
+      >
+        <Balancer>
+          Next Edu là dự án mô phỏng lại cách học hướng đến tương lai
+        </Balancer>
+      </h1>
+
+      <span className="my-4 text-base font-semibold">© NextEdu 2023</span>
+
+      <h2 className="my-4 text-xl font-bold highlight highlight-yellow-300 highlight-variant-14 dark:text-white dark:highlight-yellow-600">
+        Project Series
+      </h2>
+
+      <div className="flex items-center space-x-6">
+        <a
+          href="https://kyotomanga.live"
+          className="text-lg font-bold"
+          target="_blank"
+          rel="noreferrer"
+        >
+          kyotomanga.live
+        </a>
+
+        <a
+          href="https://realcost.shop"
+          className="text-lg font-bold"
+          target="_blank"
+          rel="noreferrer"
+        >
+          realcost.shop
+        </a>
+
+        <a
+          href="https://animanlab.online"
+          className="text-lg font-bold"
+          target="_blank"
+          rel="noreferrer"
+        >
+          animanlab.online
+        </a>
+      </div>
+    </footer>
   );
 }
