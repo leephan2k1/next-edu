953a15747b371d06cec83316dc656ebcd9433dbd
leephan2k1 <pttl2k1@gmail.com>
2023-03-27 11:27:46 +0700

    refactor: 💡 ignore compute video duration when os not support

--- src/server/helper/computeVideoDuration.ts
+++ src/server/helper/computeVideoDuration.ts
@@ -4,14 +4,23 @@ import { getVideoDurationInSeconds } from 'get-video-duration';
 import type { ChapterType } from '~/types';
 
 export async function computeVideoDuration(chapters: ChapterType[]) {
+  const supportedFFmpeg = Boolean(process.env.SUPPORTED_FFMPEG);
+
   for (let i = 0; i < chapters.length; i++) {
     if (Array.isArray(chapters[i]?.lectures)) {
       for (let j = 0; j < chapters[i]?.lectures?.length; j++) {
         for (let k = 0; k < chapters[i]?.lectures[j]?.resources.length; k++) {
           if (chapters[i]?.lectures[j]?.resources[k]?.type === 'video') {
-            const duration = await getVideoDurationInSeconds(
-              chapters[i]?.lectures[j]?.resources[k]?.url,
-            );
+            let duration;
+
+            if (supportedFFmpeg) {
+              duration = await getVideoDurationInSeconds(
+                chapters[i]?.lectures[j]?.resources[k]?.url,
+              );
+            } else {
+              //random number for fun:
+              duration = Math.floor(Math.random() * 10) + 1;
+            }
 
             Object.assign(chapters[i]?.lectures[j]?.resources[k], {
               ...chapters[i]?.lectures[j]?.resources[k],
